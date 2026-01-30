c255fbfb0248f5f9dd022feed0f66401cdff95a1
khalil <sakhalils06@gmail.com>
2026-01-30 10:56:43 +0300

    finished part a

--- hw_2.java
+++ hw_2.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+
 public class hw_2 {
     public static void main(String[] args) {
         
+    }public static int[] array_maker(int size){
+        Random random = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(101);
+        }
+        return arr;
     }
 }
+
